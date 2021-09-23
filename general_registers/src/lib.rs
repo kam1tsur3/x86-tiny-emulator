f664eb70941390c699dccdd4c908c2896db689dd
kam1tsur3 <kam1tsur3@gmail.com>
2021-09-24 02:08:26 +0900

    impl instruction 0xb8-0xbf, 0xeb

--- general_registers/src/lib.rs
+++ general_registers/src/lib.rs
@@ -1,37 +1,36 @@
+pub const EAX_INDEX :u32 = 0;
+pub const EBX_INDEX :u32 = 1;
+pub const ECX_INDEX :u32 = 2;
+pub const EDX_INDEX :u32 = 3;
+pub const ESI_INDEX :u32 = 4;
+pub const EDI_INDEX :u32 = 5;
+pub const EBP_INDEX :u32 = 6;
+pub const ESP_INDEX :u32 = 7;
+
 
 pub struct GeneralRegisters {
-    pub esp: u32, pub ebp: u32,
-    pub eax: u32, pub ebx: u32, pub ecx: u32, pub edx: u32, pub edi:u32, pub esi:u32, 
-    pub e8: u32, pub e9: u32, pub e10: u32, pub e11: u32, pub e12: u32, pub e13: u32, pub e14: u32, pub e15: u32
+    pub eax: u32, pub ebx: u32, pub ecx: u32, pub edx: u32, pub esi:u32, pub edi:u32, 
+    pub ebp: u32, pub esp: u32,
 }
 
 impl GeneralRegisters {
     pub fn new() -> GeneralRegisters {
         GeneralRegisters{
-            esp: 0x7c00, ebp: 0,
-            eax: 0, ebx: 0, ecx:0, edx:0, edi:0, esi:0,
-            e8: 0, e9: 0, e10: 0, e11: 0, e12: 0, e13: 0, e14: 0, e15: 0
+            eax: 0, ebx: 0, ecx:0, edx:0, esi:0, edi:0,
+            ebp: 0, esp: 0x7c00
         }
     }
 
     pub fn print_regs(&self) {
         println!{"------Registers------"}
-        println!("esp: 0x{:x}", self.esp);
-        println!("ebp: 0x{:x}", self.ebp);
         println!("eax: 0x{:x}", self.eax);
         println!("ebx: 0x{:x}", self.ebx);
         println!("ecx: 0x{:x}", self.ecx);
         println!("edx: 0x{:x}", self.edx);
-        println!("edi: 0x{:x}", self.edi);
         println!("esi: 0x{:x}", self.esi);
-        println!("e8 : 0x{:x}", self.e8);
-        println!("e9 : 0x{:x}", self.e9);
-        println!("e10: 0x{:x}", self.e10);
-        println!("e11: 0x{:x}", self.e11);
-        println!("e12: 0x{:x}", self.e12);
-        println!("e13: 0x{:x}", self.e13);
-        println!("e14: 0x{:x}", self.e14);
-        println!("e15: 0x{:x}", self.e15);
+        println!("edi: 0x{:x}", self.edi);
+        println!("ebp: 0x{:x}", self.ebp);
+        println!("esp: 0x{:x}", self.esp);
         println!{"--------End----------"}
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use std::fs;
+
+// lib internal
 use general_registers::GeneralRegisters;
 
-const MEMORY_SIZE = 0x1000
+const MEMORY_SIZE: usize = 0x1000;
 
 struct Emulator {
     regs: general_registers::GeneralRegisters,
     eflags: u32,
-    memory: &[u8; MEMORY_SIZE],
+    memory: Vec<u8>,
     eip: u32,
 }
 
@@ -15,15 +18,103 @@ impl Emulator {
         Emulator {
             regs: regs,
             eflags: 0,
-            memory: [0;MEMORY_SIZE],
+            memory: fs::read("memory.raw").unwrap(),
             eip: 0,
         }
     }
+
+    fn exec(&mut self) {
+        let code = self.read_mem_u8(0);
+        match code {
+            0xb8 | 0xb9 | 0xba | 0xbb |
+            0xbc | 0xbd | 0xbe | 0xbf => self.mov_r32_imm32(),
+            0xeb => self.short_jump(),
+            _ => self.not_impl(),
+        }
+    }
+
+    // instruction
+    fn not_impl(&mut self) {
+       println!("Not implemented");
+    }
+
+    fn mov_r32_imm32(&mut self) {    
+        let reg = self.read_mem_u8(0);
+        let value = self.read_mem_u32(1);
+        match (reg - 0xb8) as u32 {
+            general_registers::EAX_INDEX =>
+                self.regs.eax = value,
+            general_registers::EBX_INDEX =>
+                self.regs.ebx = value,
+            general_registers::ECX_INDEX =>
+                self.regs.ecx = value,
+            general_registers::EDX_INDEX =>
+                self.regs.edx = value,
+            general_registers::ESI_INDEX =>
+                self.regs.esi = value,
+            general_registers::EDI_INDEX =>
+                self.regs.edi = value,
+            general_registers::EBP_INDEX =>
+                self.regs.ebp = value,
+            general_registers::ESP_INDEX =>
+                self.regs.esp = value,
+            _ => {
+                println!("[*]Error in mov_r32_imm32()");
+                println!("value = {}", value)
+            },
+        }
+        self.eip += 5;
+    }
+
+    fn short_jump(&mut self) {
+        let diff = self.read_mem_i8(1);
+        self.eip = ((self.eip as i32) + (diff as i32) + 2) as u32; // 2 is length of this instruction
+    }
+
+    // memory operation
+    fn read_mem_i8(&self, offset:i32) -> i8 {
+        let index = ((self.eip as i32) + offset) as usize;
+        self.memory[index] as i8
+    }
+    
+    fn read_mem_u8(&self, offset:i32) -> u8 {
+        let index = ((self.eip as i32) + offset) as usize;
+        self.memory[index]
+    }
+
+    fn read_mem_u32(&self, offset:i32) -> u32 {
+        let index = (self.eip as i32) + offset;
+        let mut ret: u32 = 0;
+        for i in 0..4 {
+            ret |= (self.memory[(i+index) as usize] as u32) << (i*8);
+        }
+        return ret;
+    }
 }
+
+
+// test
+fn f1(i: i32) -> i32 {
+    i + 1
+}
+
+fn f2(i: i32) -> i32 {
+    i + 2
+}
+
+static tables: [fn(i32) -> i32;2] = [f1, f2];
+
 fn main() {
     let mut emulator = Emulator::new();
     emulator.regs.print_regs();
-    emulator.regs.eax = 10;
+    println!("eip: {}", emulator.eip);
+    while true {
+        emulator.exec();
+        if emulator.eip == 0 {
+            break
+        }
+    }
     emulator.regs.print_regs();
-    println!("Hello, world!");
+    println!("eip: {}", emulator.eip);
+    println!("Exit, world!");
 }
